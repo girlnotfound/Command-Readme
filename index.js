b507256db1834400ff6b256acde391164cd8c2ca
girlnotfound <150276019+girlnotfound@users.noreply.github.com>
2024-05-28 20:14:04 -0700

    Added to function write to file

--- index.js
+++ index.js
@@ -62,7 +62,15 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+        } else {
+            console.log('README.md has been generated successfully!');
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
