b7af4ec306bb2afc303e977db6eb50e29cff5105
girlnotfound <150276019+girlnotfound@users.noreply.github.com>
2024-05-28 18:51:10 -0700

    Added to index.js file, defined questions to ask the user

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Provide a short description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'Provide the steps needed to install your project:',
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Provide instructions and examples for use:',
+    },
+    {
+        type: 'input',
+        name: 'Credits',
+        message: 'If relevant, provide a list of your collaborators, with links to their GitHub profiles:',
+    },
+    {
+        type: 'list',
+        name: 'License',
+        message: 'Choose a license for your project:',
+        choices: ['Apache 2.0', 'GNUv3.0', 'MIT', 'BSD 3-Clause', 'No License'],
+    },
+    {
+        type: 'input',
+        name: 'Features',
+        message: 'If your project has features, list them here:',
+    },
+    {
+        type: 'input',
+        name: 'Contributions',
+        message: 'If you would like other developers to contribute, provide guidelines for how to do so:',
+    },
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'If you wrote tests for your application, provide examples on how to run them:',
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'Enter your email address:',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
